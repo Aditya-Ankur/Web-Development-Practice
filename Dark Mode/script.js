8b63e45ecf4a2f8062ada908a76562d2102639a2
Aditya-Ankur <aditya.cp2077v@gmail.com>
2021-07-04 13:53:59 +0530

    Added new code

---
+++ Dark Mode/script.js
@@ -0,0 +1,24 @@
+const cssDay = document.styleSheets[0];
+const cssNight = document.styleSheets[1];
+const changeModeButton = document.getElementById('dark-mode');
+
+var isDarkMode = false;
+cssDay.disabled = false;
+cssNight.disabled = true;
+
+function changeMode(){
+    if (isDarkMode == false){
+        cssDay.disabled = true;
+        cssNight.disabled = false;
+        isDarkMode = true;
+        changeModeButton.innerHTML = 'Change to Light Mode';
+    }
+    else {
+        cssDay.disabled = false;
+        cssNight.disabled = true;
+        isDarkMode = false;
+        changeModeButton.innerHTML = 'Change to Dark Mode';
+    };
+};
+
+changeModeButton.addEventListener('click', changeMode);
